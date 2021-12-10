3a30b66ea8d06fc6d51d2e1da6861322066a9566
Adelina <adelinamariacristescu@gmail.com>
2021-12-10 17:17:48 +0200

    Updates

---
+++ precache-manifest.4db7b783796e98752f6e568b904ba8c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80f007608ee3a59c665ec612e142583",
+    "url": "/React-list-of-comments-/index.html"
+  },
+  {
+    "revision": "c1a304b1716fa5e37505",
+    "url": "/React-list-of-comments-/static/js/2.b7174640.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/React-list-of-comments-/static/js/2.b7174640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a89cacb58c70256f1a5a",
+    "url": "/React-list-of-comments-/static/js/main.b717f601.chunk.js"
+  },
+  {
+    "revision": "21d2d335fe8e86ccab05",
+    "url": "/React-list-of-comments-/static/js/runtime-main.c5583f3f.js"
+  }
+]);
